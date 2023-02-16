c028feaf912b667600aebc36d1ea9eda1d5de376
LAZAR <LAZAR>
2023-02-16 15:25:28 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 
+
 /**
- * main - prints "Programming is like building a multilingual 
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline."
  * return 0.
  */
@@ -10,7 +11,9 @@
 int main(void)
 
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return(0);
+
 }
