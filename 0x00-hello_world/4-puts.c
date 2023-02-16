1bd7e306ca5ca773f8e992203ad5939f826627be
LAZAR <LAZAR>
2023-02-16 14:43:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,8 @@
 /**
  * main -prints "Programming is like building a multilingual puzzle, followed by a newline.
  * return 0.
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
