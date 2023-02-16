eccbcfa66dc3a1bac74a8a9bed1820b93ea8abf1
LAZAR <LAZAR>
2023-02-16 15:21:54 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual
+ * main - prints "Programming is like building a multilingual 
  * puzzle, followed by a newline."
  * return 0.
  */
@@ -11,5 +11,6 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return(0);
 }
