33f8f1b0921c964bd4e8b39eaa322d670cf14788
LAZAR <LAZAR>
2023-02-16 16:03:55 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <sidio.h>
 /**
- * main - Entry point
+ * main - prints "with proper grammar, but the the outcome is
+ * a piece of art, " followed newlane
+ *  Return: Always 0.
  *
- * Return: Always 0 (Success)
  */
-int main(void)
-{
 
+int main(void)
+
+{
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return(0);
 }
