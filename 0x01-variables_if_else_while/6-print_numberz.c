0fb5d37d980104b3a12073c911ec95be7580e297
LAZAR <LAZAR>
2023-02-19 13:41:00 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+/*
+* File: 6-print_numberz.c
+* Auth: LAZAR
+*/
+
+#include <stdio.h>
+/**
+* main - Prints from digit num of base 10, printf, (puts, etc... are forbidden)
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	for (i = 0 ; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+	}
+
+	putchar("\n");
+
+return (0);
+}
