64509937923b1b18554225daabd3959097e68ee7
LAZAR <LAZAR>
2023-02-21 14:33:31 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/*
+* File: 0-putchar.c
+* Auth : LAZAR
+*/
+
+#include <stdio.h>
+
+/**
+ * main - Prints
+ *
+ * Return: Alway 0.
+*/
+
+ int main(void);
+{
+	printf ("_putchar");
+	return (0);
+}
