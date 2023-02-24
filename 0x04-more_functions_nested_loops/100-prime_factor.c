553b072361e4b6874419266f449acfa05e84d7fd
LAZAR <LAZAR>
2023-02-24 15:31:26 -0800

    11 & 12 tasks update

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,83 +1,46 @@
-#include "main.h"
+#include <stdio.h>
 
-
-int _pow(int a, int b);
-
-/**
- * print_number - print the given number.
- * @n: number to print.
- */
-void print_number(int n)
-{
-	int collection = n;
-	unsigned int new_num;
-	int count = 0;
-	int print = 0;
-
-	if (n < 0)
-	{
-		new_num = -n;
-		_putchar('-');
-	}
-	else
-	{
-		new_num = n;
-	}
-
-	while (collection != 0)
-	{
-		collection /= 10;
-		++count;
-	}
-	count -= 1;
-
-	do {
-		if ((new_num / (_pow(10, count)) > 9) && count > 1)
-		{
-			print = new_num / (_pow(10, count));
-			print = print % 10;
-			_putchar('0' + print);
-		}
-		else if (count > 1)
-		{
-			print = (new_num / _pow(10, count));
-		       _putchar('0' + print);
-		}
-		else if (((new_num / 10) > 9) && count == 1)
-		{
-			print = (new_num / 10) % 10;
-			_putchar('0' + print);
-		}
-		else if (count == 1)
-		{
-			print = new_num / 10;
-			_putchar('0' + print);
-		}
-		else
-		{
-			print = new_num % 10;
-			_putchar('0' + print);
-		}
-		count--;
-	} while (count > -1);
-}
+void factor_prime(long);
 
 /**
- * _pow - power fuction
- * @a: value to use.
- * @b: growth count
+ * main - entry point
  *
- * Return: value int.
+ * Return: Always 0 (Success)
  */
-int _pow(int a, int b)
+int main(void)
 {
-	int i;
-	int p = a;
-
-	for (i = 1 ; i < b ; i++)
-	{
-		a *= p;
-	}
-	return (a);
+	factor_prime(1231952);
+	return (0);
 }
 
+/**
+ * factor_prime - prints the prime factors of a number
+ * @n: number
+ */
+
+void factor_prime(long n)
+{
+	int i, x, count, res;
+
+	for (i = n - 1; i > 0; i--)
+	{
+		count = 0;
+		x = 1;
+		while (x <= i)
+		{
+			if (i % x == 0 && i > 1)
+				count++;
+			x++;
+		}
+		if (count <= 2)
+		{
+			if (n % i == 0)
+			{
+				res = i;
+				printf("%d ", res);
+				break;
+			}
+		}
+	}
+	printf("\n");
+}
