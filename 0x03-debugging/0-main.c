1e4549ddc8dc3be2d530a4e27e66a911a8655939
LAZAR <LAZAR>
2023-02-24 09:08:13 -0800

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,3 @@
-/*
- * File: 0-main.c
- * Description: calling the function positive_or_negative fron main.h
- * Auth: LB
- */
-
 #include "main"
 
 /**
@@ -14,7 +8,7 @@
 int main(void)
 {
 	int i;
-	
+
 	i = 0;
 	positive_or_negative(i);
 	return (0);
