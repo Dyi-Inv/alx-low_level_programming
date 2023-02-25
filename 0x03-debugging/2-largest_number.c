b2d20555b2dcb808348baf2ff1daf9dbe7cba40d
LAZAR <LAZAR>
2023-02-24 16:22:29 -0800

    0x03-debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -18,21 +18,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	largest = bigger_number(a, bigger_number(b, c));
-	return (largest);
-}
-
-/**
- * bigger_number - bigger number of two
- * @a: first integer
- * @b: second integer
- * Return: Bigger number
- */
-int bigger_number(int a, int b)
-{
-
-	if (a > b)
-		return (a);
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
 	else
-		return (b);
+	{
+		largest = c;
+	}
+
+	return (largest);
 }
